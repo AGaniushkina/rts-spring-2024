61af65987ada8c53c328a98da82bad0dc57725de
AGaniushkina <annaganyushkina02@gmail.com>
2024-03-30 14:54:11 +0400

    first commit

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,33 @@
-void setup() {
+const int ledPins[] = { 3, 5, 6, 9, 10 };
+int pinsCount = 5;
+int ledStates[] = { HIGH, HIGH, HIGH, HIGH, HIGH };
 
+unsigned long previousMicros[] = { 0, 0, 0, 0, 0 };
+
+unsigned long interval[] = { 10000, 1000, 500, 100, 50 };
+
+void setup() {
+  for (int thisPin = 0; thisPin < pinsCount; thisPin++) {
+    pinMode(ledPins[thisPin], OUTPUT);
+  }
 }
 
 void loop() {
 
+  unsigned long currentMicros = micros();
+
+  for (int thisPin = 0; thisPin < pinsCount; thisPin++) {
+    if (currentMicros - previousMicros[thisPin] >= interval[thisPin]) {
+
+      previousMicros[thisPin] = currentMicros;
+
+      if (ledStates[thisPin] == LOW) {
+        ledStates[thisPin] = HIGH;
+      } else {
+        ledStates[thisPin] = LOW;
+      }
+
+      digitalWrite(ledPins[thisPin], ledStates[thisPin]);
+    }
+  }
 }
--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,7 +1,33 @@
-void setup() {
+#include <Arduino.h>
+#include <TaskManagerIO.h>
 
+const int pinsCount = 5;
+const int ledPins[pinsCount] = {3, 5, 6, 9, 10};
+const TimePeriod interval[pinsCount] = {repeatMillis(10), repeatMillis(1), repeatMicros(500), repeatMicros(100), repeatMicros(50)};
+
+taskid_t tasks[pinsCount];
+
+class Exec : public Executable {
+private:
+  int _state = LOW;
+
+public:
+  int pin;
+  void exec() override {
+      digitalWrite(pin, _state);
+      _state = (_state == LOW) ? HIGH : LOW;
+  }
+} execs[pinsCount];
+
+void setup() {
+  for (int i = 0; i < pinsCount; ++i) {
+    execs[i] = Exec();
+    execs[i].pin = ledPins[i];
+    pinMode(ledPins[i], OUTPUT);
+    tasks[i] = taskManager.schedule(interval[i], &execs[i]);
+  }
 }
 
 void loop() {
-
+  taskManager.runLoop();
 }
